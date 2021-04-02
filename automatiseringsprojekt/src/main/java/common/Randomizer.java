544d664749ba22051fd2fce53d1b2162fe811322
Daniel <foebane@hotmail.com>
2021-04-02 23:03:38 +0200

    Random 100 added

--- automatiseringsprojekt/src/main/java/common/Randomizer.java
+++ automatiseringsprojekt/src/main/java/common/Randomizer.java
@@ -1,20 +1,19 @@
 package common;
 
-
-
 import java.util.concurrent.ThreadLocalRandom;
-
-
+import java.util.Random;
 
 public class Randomizer {
 
-	private int randomNumbers = ThreadLocalRandom.current().nextInt();
+	Random num = new Random();
 
+	private int randomNumbers = ThreadLocalRandom.current().nextInt();
+	String onehundred = "abcde".repeat(20);
 
 	public String mail(String email) {
 
 		if (email.equals("validEmail")) {
-			email = ("Smirk" + String.valueOf(randomNumbers) + "@hotmail.com");
+			email = ("Greg" + String.valueOf(randomNumbers) + "@hotmail.com");
 
 		} else if (email.equals("noEmail")) {
 			email = "";
@@ -27,7 +26,9 @@ public class Randomizer {
 	public String user(String username) {
 
 		if (username.equals("validUserName")) {
-			username = ("John" + String.valueOf(randomNumbers) + "@Doe.com");
+			username = ("MrT" + String.valueOf(randomNumbers) + "@hotmail.com");
+		} else if (username.equals("tooManyCharacters")) {
+			username = (onehundred);
 		}
 
 		return username;
