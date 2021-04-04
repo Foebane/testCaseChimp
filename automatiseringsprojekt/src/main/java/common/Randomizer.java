ab5c1882f16c3a3feffcf5171faa105f72e21dbd
Daniel <foebane@hotmail.com>
2021-04-04 12:43:19 +0200

    Changed some public to private. Changed else if to else

--- automatiseringsprojekt/src/main/java/common/Randomizer.java
+++ automatiseringsprojekt/src/main/java/common/Randomizer.java
@@ -1,19 +1,19 @@
 package common;
 
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.Random;
+
 
 public class Randomizer {
 
-	Random num = new Random();
+	
 
 	private int randomNumbers = ThreadLocalRandom.current().nextInt();
-	String onehundred = "abcde".repeat(30);
+	private String itsOverOneHundred = "abcde".repeat(30);
 
 	public String mail(String email) {
 
 		if (email.equals("validEmail")) {
-			email = ("Greg" + String.valueOf(randomNumbers) + "@hotmail.com");
+			email = ("Test" + String.valueOf(randomNumbers) + "@hotmail.com");
 
 		} else if (email.equals("noEmail")) {
 			email = "";
@@ -26,9 +26,9 @@ public class Randomizer {
 	public String user(String username) {
 
 		if (username.equals("validUserName")) {
-			username = ("MrT" + String.valueOf(randomNumbers) + "@hotmail.com");
+			username = ("Ouch" + String.valueOf(randomNumbers) + "@hotmail.com");
 		} else if (username.equals("tooManyCharacters")) {
-			username = (onehundred);
+			username = (itsOverOneHundred);
 		}
 
 		return username;
--- automatiseringsprojekt/src/test/java/stepDefinitions/StepDefinitions.java
+++ automatiseringsprojekt/src/test/java/stepDefinitions/StepDefinitions.java
@@ -13,7 +13,7 @@ public class StepDefinitions {
 
 	private WebDriver driver;
 	private Randomizer rndm;
-	WebElement result;
+	private WebElement result;
 
 	@Given("I have opened {string} , navigated to https:\\/\\/login.mailchimp.com\\/signup\\/ and accepted cookies")
 	public void i_have_opened_navigated_to_https_login_mailchimp_com_signup_and_accepted_cookies(String browser)
@@ -57,23 +57,18 @@ public class StepDefinitions {
 
 	@Then("the correct {string} should be shown on the screen")
 	public void the_correct_should_be_shown_on_the_screen(String message) {
-                 
-		
-			if(message.equals("Check your email")) {
-				result = driver.findElement(By.cssSelector(".\\!margin-bottom--lv3"));
-				
-			}
-			else if(message.equals("Enter a value less than 100 characters long") || message.equals("Another user with this username already exists. Maybe it's your evil twin. Spooky.") || message.equals("Please enter a value")) {
-				result = driver.findElement(By.cssSelector(".invalid-error"));
-				
-			}
-			
-			
-			assertEquals(message, result.getAttribute("innerText"));
-			System.out.println(result.getAttribute("innerText") +"= "+ message);
-		
 
-		
+		if (message.equals("Check your email")) {
+			result = driver.findElement(By.cssSelector(".\\!margin-bottom--lv3"));
+
+		} else {
+			result = driver.findElement(By.cssSelector(".invalid-error"));
+
+		}
+
+		assertEquals(message, result.getAttribute("innerText"));
+		System.out.println(result.getAttribute("innerText") + " = " + message);
+
 		driver.quit();
 	}
 
